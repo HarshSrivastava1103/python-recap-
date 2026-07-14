0d548b2d408d0a2ba436c3cfdabd8bb90362a36c
Harsh Srivastava <142967284+HarshSrivastava1103@users.noreply.github.com>
2026-07-14 22:51:32 +0530

    home

---
+++ input.py
@@ -0,0 +1,11 @@
+print ("Hellp world")
+
+s = "Harsh"
+print (s)
+
+#We can use the print() function to print single and multiple variables. 
+#We can print multiple variables by separating them with commas.
+s = "India"
+age = 23 
+city = "kanpur" 
+print(s, age, city)
